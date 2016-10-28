db40bbf0d28cfbde2abff7a28106c77bfe3048e1
arun teja <training.saibersys9@gmail.com>
2016-10-28 16:35:11 -0500

    Add files via upload

---
+++ phparray.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+</head>
+<title>Hello world</title>
+<body>
+<?php
+        
+
+          $people = array("Alice","Bob","catherine");
+		  
+		  $number = array(5,3,7);
+		  $sum = 0;
+		  
+		  foreach($number as $number){
+			  $sum = $sum + $number;
+		  }
+		  
+		  echo $sum;
+		  
+		}
+ ?>
+</body>
+</html>
